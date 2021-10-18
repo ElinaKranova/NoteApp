dbc6cfc9c4a7b77466f9b9a9a8ba930fb339c7da
EpicItami <elinixelka@mail.ru>
2021-10-19 04:52:29 +0700

    Реализование классы логики, добавить xml-комментарии к классам, реализовация сохранения и загрузки данных программы с помощью механизма сериализации

--- NoteApp/Project.cs
+++ NoteApp/Project.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace NoteApp
 {
+    /// <summary>
+    /// Класс, хранящий данные о заметках
+    /// </summary>
     public class Project
     {
+        /// <summary>
+        /// Список заметок.
+        /// </summary>
+        private List<Note> _notes;
+
+        /// <summary>
+        /// Возвращает и задает список заметок.
+        /// </summary>
+        public List<Note> Notes
+        {
+            get { return _notes; }
+            set
+            {
+                _notes = value;
+            }
+        }
     }
 }
